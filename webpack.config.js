1dd9011f2736cd75e9db8e61c5f4f4639b209bd4
piaorinan <piaorinan@chinaath.com>
2022-02-07 15:13:47 +0800

    2022-02-07 15:13:47 default commit message

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,11 @@ const path                   = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  // mode   : 'development',
   entry  : './src/index.js',
   devtool: 'source-map',
   output : {
-    filename: 'dlog-node.js',
-    // 必须是一个绝对路径
-    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, './build'),
   },
   module : {
     rules: [],
